a1bf034e645aafc70aeb75fa323c169a56887bba
Marcelo Fortuna <celofortuna@gmail.com>
2023-11-16 19:59:18 -0300

    Reverting the last commit

--- refresh_commands.js
+++ refresh_commands.js
@@ -3,29 +3,18 @@ import { PRIVATE_COMMANDS } from "./commands/private/private_commands_router.js"
 import "dotenv/config";
 import { REST, Routes } from "discord.js";
 
-async function loadCommands(appId, commands, isPrivateCommands) {
+async function loadCommands(appId, commands) {
   const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
-  if (isPrivateCommands === true) {
-    console.log('Refreshing private (/) commands.');
+  console.log('Refreshing application (/) commands.');
 
-    try {
-      await rest.put(Routes.applicationCommands(appId, { body: [commands] }));
-      console.log('Refreshed private (/) commands! :)\n');
-    } catch (e) {
-      console.error(`Erro ao inicializar comandos privados\nMensagem: ${e}`);
-    }
-  } else {
-    console.log('Refreshing global (/) commands.');
-
-    try {
-      await rest.put(Routes.applicationCommands(appId, { body: [commands] }));
-      console.log('Refreshed global (/) commands! :)');
-    } catch (e) {
-      console.error(`Erro ao inicializar comandos globais\nMensagem: ${e}`);
-    }
+  try {
+    await rest.put(Routes.applicationCommands(appId, { body: [commands] }));
+    console.log('Refreshed application (/) commands! :)');
+  } catch (e) {
+    console.error(`Erro ao inicializar comandos globais\nMensagem: ${e}`);
   }
 }
 
-await loadCommands(process.env.APP_ID, GLOBAL_COMMANDS, false);
-await loadCommands(process.env.APP_ID, PRIVATE_COMMANDS, true);
+await loadCommands(process.env.APP_ID, GLOBAL_COMMANDS);
+await loadCommands(process.env.APP_ID, PRIVATE_COMMANDS)
